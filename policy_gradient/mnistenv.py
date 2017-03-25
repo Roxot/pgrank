ef682b7c4203ae8ca77b328397171f28e2cbf509
Bryan Eikema <bryan.eikema@student.uva.nl>
2017-03-25 13:36:37 +0100

    Moved MNISTEnvironment to its own file again

--- policy_gradient/mnistenv.py
+++ policy_gradient/mnistenv.py
@@ -9,8 +9,8 @@ class MNISTEnvironment:
 
     def __init__(self):
         self.dataset = input_data.read_data_sets("MNIST_data/", one_hot=True)
-        self.action_space = spaces.Discrete(10)
-        self.observation_space = spaces.Box(low=0, high=255, shape=(784,))
+        # self.action_space = spaces.Discrete(10)
+        # self.observation_space = spaces.Box(low=0, high=255, shape=(784,))
 
     def _observe(self):
         batch = self.dataset.train.next_batch(1)
@@ -19,11 +19,27 @@ class MNISTEnvironment:
         label = np.where(label_onehot == 1)[0][0]
         return observation, label
 
+    def _reward(self, predicted_label):
+        # if predicted_label == 3 and self.current_label == 5:
+        #     return -10
+        # elif predicted_label == 5 and self.current_label == 5:
+        #     return 10
+        # if predicted_label == 8 and self.current_label == 5:
+        #     return self.NEG_REWARD * 10
+        # elif predicted_label == 5 and self.current_label == 5:
+        #     return self.POS_REWARD * 10
+        # else:
+        return self.POS_REWARD if predicted_label == self.current_label else self.NEG_REWARD
+
+
     def step(self, label):
-        reward = self.POS_REWARD if label == self.current_label else self.NEG_REWARD
+        reward = self._reward(label)
         new_observation, self.current_label = self._observe()
         return new_observation, reward
 
     def reset(self):
         observation, self.current_label = self._observe()
         return observation
+
+    def test_set(self):
+        return self.dataset.test.images, self.dataset.test.labels
--- policy_gradient/pgmnist.py
+++ policy_gradient/pgmnist.py
@@ -1,55 +1,13 @@
-from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.contrib.layers import initializers
 from tensorflow.contrib.layers import regularizers
 from sklearn.metrics import confusion_matrix
 
 from plot import plot_confusion_matrix
+from mnistenv import MNISTEnvironment
 
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-class MNISTEnvironment:
-
-    POS_REWARD = 1
-    NEG_REWARD = -1
-
-    def __init__(self):
-        self.dataset = input_data.read_data_sets("MNIST_data/", one_hot=True)
-        # self.action_space = spaces.Discrete(10)
-        # self.observation_space = spaces.Box(low=0, high=255, shape=(784,))
-
-    def _observe(self):
-        batch = self.dataset.train.next_batch(1)
-        observation = batch[0]
-        label_onehot = batch[1][0]
-        label = np.where(label_onehot == 1)[0][0]
-        return observation, label
-
-    def _reward(self, predicted_label):
-        # if predicted_label == 3 and self.current_label == 5:
-        #     return -10
-        # elif predicted_label == 5 and self.current_label == 5:
-        #     return 10
-        # if predicted_label == 8 and self.current_label == 5:
-        #     return self.NEG_REWARD * 10
-        # elif predicted_label == 5 and self.current_label == 5:
-        #     return self.POS_REWARD * 10
-        # else:
-        return self.POS_REWARD if predicted_label == self.current_label else self.NEG_REWARD
-
-
-    def step(self, label):
-        reward = self._reward(label)
-        new_observation, self.current_label = self._observe()
-        return new_observation, reward
-
-    def reset(self):
-        observation, self.current_label = self._observe()
-        return observation
-
-    def test_set(self):
-        return self.dataset.test.images, self.dataset.test.labels
 
 # Dataset parameters
 classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
