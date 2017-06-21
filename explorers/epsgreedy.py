7be3cc756c391cfe8f765808d62f049699b606d7
Bryan Eikema <bryan.eikema@student.uva.nl>
2017-06-21 18:29:04 +0200

    Now does epsilon greedy exploration on single batch elements instead of entire batches

--- explorers/epsgreedy.py
+++ explorers/epsgreedy.py
@@ -11,9 +11,16 @@ class EpsGreedy:
         self.greedy_action = greedy_action
         self.explore_action = explore_action
 
-    # TODO should we explore/exploit on the entire batch?
     def rank_docs(self, policy):
-        if np.random.rand() < self.epsilon:
-            return self.explore_action(policy)
-        else:
-            return self.greedy_action(policy)
+
+        # Decide which batch elements we explore and exploit.
+        rand_unif = np.random.rand(policy.shape[0], 1)
+        should_explore = np.tile((rand_unif < self.epsilon).astype(int), policy.shape[1])
+        should_exploit = np.tile((rand_unif >= self.epsilon).astype(int), policy.shape[1])
+
+        # Perform both the greedy as wel as the exploratory actions.
+        exploratory_batch = self.explore_action(policy)
+        greedy_batch = self.greedy_action(policy)
+
+        # Return the batch balanced between exploration and exploitation.
+        return exploratory_batch * should_explore + greedy_batch * should_exploit
